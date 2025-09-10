f6d669a75ca24b6a6903608380bfb4e726b5f4c8
bintoufha <bintoufha@gmail.com>
2025-09-10 21:53:21 +0200

    suite du mouvement de stock

--- src/main/java/com/bintoufha/gestionStocks/services/Impl/VenteServiceImpl.java
+++ src/main/java/com/bintoufha/gestionStocks/services/Impl/VenteServiceImpl.java
@@ -1,11 +1,16 @@
 package com.bintoufha.gestionStocks.services.Impl;
 
 import com.bintoufha.gestionStocks.dto.VentesDto;
+import com.bintoufha.gestionStocks.exception.ErrorCodes;
+import com.bintoufha.gestionStocks.exception.InvalEntityException;
 import com.bintoufha.gestionStocks.services.VenteService;
+import com.bintoufha.gestionStocks.validator.VenteValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.stylesheets.LinkStyle;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -14,8 +19,14 @@ public class  VenteServiceImpl implements VenteService {
 
     @Override
     public VentesDto save(VentesDto ventesDto) {
-        List<String> errors = VentV
+        List<String> errors = VenteValidator.validate(ventesDto);
+        if (!errors.isEmpty()){
+            log.warn("vente n'est pas valide");
+            throw new InvalEntityException("l'objet vente n'est pas valide", ErrorCodes.VENTE_NOT_VALID,errors);
+        }
 
+        List<String> articleErrors = new ArrayList<>();
+        ventesDto.
         return null;
     }
 
